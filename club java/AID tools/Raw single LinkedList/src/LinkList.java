ccd70602e28d67bd942a0fdc4536003955a413e0
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-08-17 22:54:48 -0700

    python and Java competitive programming questions

--- club java/AID tools/Raw single LinkedList/src/LinkList.java
+++ club java/AID tools/Raw single LinkedList/src/LinkList.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 // Definition for singly-linked list class.
 public class LinkList {
     /* Node Class */
+    ListNode head; // head of list
     private static class ListNode {
         int val;
         ListNode next;
@@ -17,7 +18,7 @@ public class LinkList {
 
     public static void main(String[] args){
         // test drivers //
-        /** Raw LinkedList **/
+        /** Raw LinkedList: method 1 **/
         ListNode list1 = new ListNode();
         ListNode dummy = list1;
 
@@ -42,7 +43,7 @@ public class LinkList {
         dummy.next = num4;
         dummy = dummy.next; // same as <dummy = num4>
 
-        myFuncRawLinkedList(list1.next);
+        myFuncRawLinkedList_M1(list1.next);
 
         /** build-in LinkedList **/
         // add new nodes to the linked list
@@ -53,23 +54,50 @@ public class LinkList {
         list2.add(9);
 
         myFuncBuildInLinkedList(list2);
+
+        /** Raw LinkedList: method 2 **/
+        // declare LinkList
+        LinkList list3 = new LinkList();
+
+        // declare ListNode
+        ListNode num5 = new ListNode(5);
+        ListNode num6 = new ListNode(6);
+        ListNode num7 = new ListNode(7);
+
+        // connect node
+        list3.head = num5;
+        num5.next = num6;
+        num6.next = num7;
+
+        /*myFuncRawLinkedList_M1(list3.head);*/
+        myFuncRawLinkedList_M2(list3);
     }
 
-    public static void myFuncRawLinkedList(ListNode list){
+    public static void myFuncRawLinkedList_M1(ListNode list){
         // print list
         while(list != null){
             System.out.println(list.val);
             list = list.next;
         }
-        System.out.println("=== End Raw LinkedList === \n");
+        System.out.println("=== End Raw LinkedList_M1 === \n");
     }
 
-    public static void myFuncBuildInLinkedList(java.util.LinkedList list){
+    public static void myFuncBuildInLinkedList(LinkedList list){
         // print list
         while(!list.isEmpty()){
             System.out.println(list.pop());
         }
         System.out.println("=== End build-in LinkedList === \n");
+    }
+
+    public static void myFuncRawLinkedList_M2(LinkList list){
+        ListNode n = list.head;
+        // print list
+        while(n != null){
+            System.out.println(n.val);
+            n = n.next;
+        }
+        System.out.println("=== End Raw LinkedList_M2 === \n");
     }
 } // LinkList
 
