60b8b00580adc5e69e13d364fd306701638b9454
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-09-10 08:53:26 -0700

    Java competitive programming questions

---
+++ club java/HappyNumber/src/HappyNumber.java
@@ -0,0 +1,123 @@
+import java.lang.*;
+import java.util.HashSet;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        /**
+         * Test case 1:
+         * Purpose: help to determine one digit number that converge to true
+         * Use the result to define based case
+         * Result: 1 and 7 will converge to true, and the others will converge to false.
+         * **/
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+            System.out.println(isHappy2(i));
+        }
+
+        System.out.println("\n==== = end testcase1 =====\n");
+
+        int n1 = 19;
+        int n2 = 2;
+        int n3 = 101;
+        int n4 = 39;
+        int n5 = 7;
+        int n6 = 1111111;
+
+        System.out.println(isHappy1(n1)); // true
+        System.out.println(isHappy1(n2)); // false
+        System.out.println(isHappy1(n3)); // false
+        System.out.println(isHappy1(n4)); // false
+        System.out.println(isHappy1(n5)); // true
+        System.out.println(isHappy1(n6)); // true
+
+        System.out.println("\n==== = end testcase2 =====\n");
+
+        System.out.println(isHappy2(n1)); // true
+        System.out.println(isHappy2(n2)); // false
+        System.out.println(isHappy2(n3)); // false
+        System.out.println(isHappy2(n4)); // false
+        System.out.println(isHappy2(n5)); // true
+        System.out.println(isHappy2(n6)); // true
+
+        System.out.println("\n==== = end testcase3 =====\n");
+
+    }
+
+    /*
+     * Link: https://leetcode.com/problems/happy-number/
+     * Purpose: Determine if it is a happy number:
+     *        : Starting with any positive integer, replace the number by the sum of the squares of its digits.
+              : Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+              : Those numbers for which this process ends in 1 are happy.
+     * Parameters: num n - a number to check if happy
+     * Returns: boolean - true if happy. Otherwise false
+     * Pre-Condition: 1 <= n <= 231 - 1
+       Post-Condition : None
+     */
+
+    // Dont use hash map
+    public static boolean isHappy1(int n){
+        char[] digits;
+        do{
+            // store sum
+            int sum = 0;
+
+            // store each digit in char array
+            digits = Integer.toString(n).toCharArray();
+
+            // the sum of the squares of all digits'
+            for (int digit:digits){
+                sum += Math.pow(Character.getNumericValue(digit), 2);
+            }
+
+            // virtual aid
+            // System.out.println(sum);
+
+            // the only single digit that converge to true are 1 and 7
+            if(sum == 1 || sum == 7){
+                return true;
+            }
+
+            // set n to sum to prepare for the next iteration
+            n = sum;
+        }while(Integer.toString(n).toCharArray().length != 1);
+
+        return false;
+    }
+
+    // use Hash map b/c just can (same logic no different): to check if the number is already found
+    public static boolean isHappy2(int n){
+        char[] digits;
+        HashSet<Integer> seen = new HashSet<>();
+        do{
+            // store sum
+            int sum = 0;
+
+            // store each digit in char array
+            digits = Integer.toString(n).toCharArray();
+
+
+            // the sum of the squares of all digits'
+            for (int digit:digits){
+                sum += Math.pow(Character.getNumericValue(digit), 2);
+            }
+
+            // the only single digit that converge to true are 1 and 7
+            if(sum == 1 || sum == 7){
+                return true;
+            }
+
+            // duplicate sum have found and we struck in a loop
+            if(seen.contains(sum)){
+                return false;
+            }
+
+            // add seen num
+            seen.add(sum);
+
+            // set n to sum to prepare for the next iteration
+            n = sum;
+        }while(Integer.toString(n).toCharArray().length != 1);
+        return false;
+    }
+}
---
+++ club java/Revert Linked List/src/RevertLinkedList.java
@@ -0,0 +1,95 @@
+import java.util.LinkedList;
+
+// Definition for singly-linked list class.
+public class RevertLinkedList {
+    /* Node Class */
+    ListNode head; // head of list
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        // Constructor to create a new node
+        ListNode() {}
+        ListNode(int d) {
+            val = d;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args){
+        /** Raw Test Case **/
+        // declare LinkList
+        RevertLinkedList list3 = new RevertLinkedList();
+
+        // declare ListNode
+        ListNode num1 = new ListNode(7);
+        ListNode num2 = new ListNode(6);
+        ListNode num3 = new ListNode(5);
+        ListNode num4 = new ListNode(4);
+        ListNode num5 = new ListNode(3);
+        ListNode num6 = new ListNode(2);
+        ListNode num7 = new ListNode(1);
+
+        // connect node
+        list3.head = num1;
+        num1.next = num2;
+        num2.next = num3;
+        num3.next = num4;
+        num4.next = num5;
+        num5.next = num6;
+        num6.next = num7;
+
+        myFuncRawLinkedList_M1(revertLinkedList(list3.head));
+    }
+
+    /*
+     * Link: https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3966/
+     * Purpose: Find a reversed Linked list
+     * Parameters: ListNode head - a head pointer to a single linked list
+     * Returns: ListNode reversedList - a head pointer to a reversed single linked list
+     * Pre-Condition: The number of nodes in the list is the range [0, 5000].
+                    : -5000 <= Node.val <= 5000
+       Post-Condition : None
+     */
+    public static ListNode revertLinkedList(ListNode head) {
+        // no need to reverse if head is null or there is only 1 node.
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        /** remomber these 3 lines **/
+        ListNode reversedList = head;
+        ListNode toDo = head.next;
+
+        // initialize a reversed list
+        reversedList.next = null;
+        /** remomber these 3 lines **/
+
+        while(toDo != null){
+            // re-initialize/initialize helping node
+            ListNode dummy = toDo;
+
+            // point toDo to the next node
+            toDo = dummy.next;
+
+            // link the secound element to a reversed list
+            dummy.next = reversedList;
+
+            // move reversedList to the head of the list
+            reversedList = dummy;
+        }
+
+
+        return reversedList;
+    }
+
+    public static void myFuncRawLinkedList_M1(ListNode list){
+        // print list
+        while(list != null){
+            System.out.println(list.val);
+            list = list.next;
+        }
+        System.out.println("=== End Raw LinkedList_M1 === \n");
+    }
+} // LinkList
+
