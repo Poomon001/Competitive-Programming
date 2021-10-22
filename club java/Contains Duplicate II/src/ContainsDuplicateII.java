ab4cfa8170998945ea7fb2808f4101174934d2b7
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-10-22 05:03:08 -0700

    java competitive programming questions

--- club java/Contains Duplicate II/src/ContainsDuplicateII.java
+++ club java/Contains Duplicate II/src/ContainsDuplicateII.java
@@ -6,13 +6,32 @@ public class ContainsDuplicateII {
         int[] n2 = {1,0,1,1};
         int[] n3 = {1,2,3,1,2,3};
         int[] n4 = {99, 99};
-        System.out.println(containsNearbyDuplicate(n1, 3)); // true
-        System.out.println(containsNearbyDuplicate(n2, 1)); // true
-        System.out.println(containsNearbyDuplicate(n3, 2)); // false
-        System.out.println(containsNearbyDuplicate(n4, 2)); // true
-        System.out.println(containsNearbyDuplicate(n4, 1)); // true
+        System.out.println("\n === optimized memory solution === \n");
+        System.out.println(containsNearbyDuplicate_M1(n1, 3)); // true
+        System.out.println(containsNearbyDuplicate_M1(n2, 1)); // true
+        System.out.println(containsNearbyDuplicate_M1(n3, 2)); // false
+        System.out.println(containsNearbyDuplicate_M1(n4, 2)); // true
+        System.out.println(containsNearbyDuplicate_M1(n4, 1)); // true
+
+        System.out.println("\n === optimized runtime solution === \n");
+        System.out.println(containsNearbyDuplicate_M2(n1, 3)); // true
+        System.out.println(containsNearbyDuplicate_M2(n2, 1)); // true
+        System.out.println(containsNearbyDuplicate_M2(n3, 2)); // false
+        System.out.println(containsNearbyDuplicate_M2(n4, 2)); // true
+        System.out.println(containsNearbyDuplicate_M2(n4, 1)); // true
     }
 
+    /**
+     * Link: https://leetcode.com/problems/contains-duplicate-ii/
+     * Purpose: Find if an array contain any duplicate in a range of [the current index i, to index i+k]
+     * Parameters: int[] nums - an array of int
+     *           : int k - a given range
+     * Returns: boolean - true if conatains a duplicate in a range of [the current index i, to index i+k]. Otherwise false
+     * Pre-Condition: 1 <= nums.length <= 105
+     *              : -109 <= nums[i] <= 109
+     *              : 0 <= k <= 105
+     * Post-Condition : None
+     **/
     // Optimized memory solution - runtime: O(n^2), memory: O(1)
     public static boolean containsNearbyDuplicate_M1(int[] nums, int k) {
         int i = 0;
@@ -34,8 +53,19 @@ public class ContainsDuplicateII {
         return false;
     }
 
+    /**
+     * Link: https://leetcode.com/problems/contains-duplicate-ii/
+     * Purpose: Find if an array contain any duplicate in a range of [the current index i, to index i+k]
+     * Parameters: int[] nums - an array of int
+     *           : int k - a given range
+     * Returns: boolean - true if conatains a duplicate in a range of [the current index i, to index i+k]. Otherwise false
+     * Pre-Condition: 1 <= nums.length <= 105
+     *              : -109 <= nums[i] <= 109
+     *              : 0 <= k <= 105
+     * Post-Condition : None
+     **/
     // Optimized runtime solution - runtime: O(n), memory: O(n)
-    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+    public static boolean containsNearbyDuplicate_M2(int[] nums, int k) {
         // {num, index that is found}
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
