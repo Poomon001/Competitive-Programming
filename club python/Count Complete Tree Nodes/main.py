3f7dcaca2b3414b1d378f34324072da93aef8355
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-10-24 06:17:14 -0700

    python competitive programming questions

---
+++ club python/Count Complete Tree Nodes/main.py
@@ -0,0 +1,91 @@
+# Python program to insert element in binary tree
+from idlelib.tree import TreeNode
+from typing import Optional, List
+
+class newNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+'''
+    Link: https://leetcode.com/problems/count-complete-tree-nodes/
+    Purpose: Count all nodes in a completed tree 
+    parameter: Optional[TreeNode] root - a root of a completed binary tree
+    return: int count - number of all nodes in the tree
+    Pre-Condition: The number of nodes in the tree is in the range [0, 5 * 104].
+                 : 0 <= Node.val <= 5 * 104
+                 : The tree is guaranteed to be complete.
+    Post-Condition: none
+'''
+# runtime: O(n), memory: O(1)
+def countNodes(root):
+    count = 0
+
+    def dfs(root):
+        if root is None:
+            return
+
+        nonlocal count
+        count += 1
+
+        dfs(root.left)
+        dfs(root.right)
+
+    dfs(root)
+    return count
+
+# Credit: https://www.geeksforgeeks.org/insertion-in-a-binary-tree-in-level-order/
+""" Inorder traversal of a binary tree"""
+def inorder(temp):
+    if (not temp):
+        return
+
+    inorder(temp.left)
+    print(temp.key, end=" ")
+    inorder(temp.right)
+
+# Credit: https://www.geeksforgeeks.org/insertion-in-a-binary-tree-in-level-order/
+"""function to insert element in binary tree """
+def insert(temp, key):
+    if not temp:
+        root = newNode(key)
+        return
+    q = []
+    q.append(temp)
+
+    # Do level order traversal until we find
+    # an empty place.
+    while (len(q)):
+        temp = q[0]
+        q.pop(0)
+
+        if (not temp.left):
+            temp.left = newNode(key)
+            break
+        else:
+            q.append(temp.left)
+
+        if (not temp.right):
+            temp.right = newNode(key)
+            break
+        else:
+            q.append(temp.right)
+
+
+if __name__ == "__main__":
+    root = newNode(1)
+    root.left = newNode(2)
+    root.right = newNode(3)
+
+    print(countNodes(root)) # 3
+
+    root.left.left = newNode(4)
+    root.left.right = newNode(5)
+    root.right.left = newNode(6)
+
+    print(countNodes(root)) # 6
+
+    root.right.right = newNode(7)
+
+    print(countNodes(root)) # 7
