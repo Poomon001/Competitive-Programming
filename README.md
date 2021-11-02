9ca509012354360a7a085c432cf0bc1cc8a68ab0
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-11-02 04:25:50 -0700

    python and mySQL competitive programming questions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Competitive-Programming
-A list of competitive programming problems that I solved with Java and Python in my spare time.
+A list of competitive programming problems that I solved with Java, Python, and mySQL in my spare time.
--- club mySQL/Customers Who Never Order/main.py
+++ club mySQL/Customers Who Never Order/main.py
@@ -1,4 +1,57 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+# Setup database
+load_dotenv()
+db = mysql.connector.connect(host=os.getenv("HOST"), user=os.getenv("USER"), passwd=os.getenv("PASSWD"),
+                                 database=os.getenv("DATABASE"))
+
+mycursor = db.cursor()
+
+'''
+    Link: https://leetcode.com/problems/customers-who-never-order/
+    Purpose: Find a customer who never ordered from mySQL database in any order
+    parameter: none
+    return: none
+    Pre-Condition: none
+    Post-Condition: none
+'''
+def customersWhoNeverOrder():
+    print("\n+===== Answer =====+")
+    mycursor.execute("SELECT name AS Customers FROM Customers WHERE id NOT IN (SELECT customerId FROM Orders)")
+    for data in mycursor:
+        print(data)
 
 if __name__ == '__main__':
-    db = mysql.connector.connect(host="...", user="...", passwd="...", database="database_that_will_be_used")
+    # setup a question
+    ''' create database *(execute once) '''
+    # mycursor.execute("CREATE DATABASE customers_who_never_order")
+
+    ''' create tables *(execute once)'''
+    # mycursor.execute("CREATE TABLE Customers( name VARCHAR (50), id int PRIMARY KEY AUTO_INCREMENT)")
+    # mycursor.execute("CREATE TABLE Orders( customerId int, id int PRIMARY KEY AUTO_INCREMENT)")
+
+    ''' Insert data row *(execute once)'''
+    # customers = (("Joe",), ("Henry",), ("Sam",), ("Max",))
+    # mycursor.executemany("INSERT INTO Customers(name) VALUES (%s)", customers)
+    # db.commit()
+
+    # orders = ((3,), (1,))
+    # mycursor.executemany("INSERT INTO Orders(customerId) VALUES (%s)", orders)
+    # db.commit()
+
+    ''' see tables '''
+    print("\n+===== Customers Table ====+")
+    mycursor.execute("SELECT * FROM Customers")
+    for data in mycursor:
+        print(data)
+
+    print("\n+===== Orders Table ====+")
+    mycursor.execute("SELECT * FROM Orders")
+    for data in mycursor:
+        print(data)
+
+    customersWhoNeverOrder()
+
+
