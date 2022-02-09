91cbe4191a0dfc3f60e0b56bd381257dc46ba9e6
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2022-02-09 07:34:49 -0800

    java competitive programming questions

--- club java/Sort An Array/src/SortAnArray.java
+++ club java/Sort An Array/src/SortAnArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SortAnArray {
     /**
      * Link: https://leetcode.com/problems/sort-an-array/
@@ -73,31 +75,100 @@ public class SortAnArray {
         System.out.println("\n+=== quick sort ===+\n");
         int[] nums10 = {5,1,1,2,0,8,9,0};
         int[] nums11 = {3,9,8,4};
-        int[] nums12 = {64,25,12,22,11,-10};
+        int[] nums12 = {};
         int[] nums13 = {64,25,12,22,11,-10, 78};
         int[] nums14 = {0,1,2,3,4,5,6,7};
 
-//        for(int i:quickSort(nums10, 0, nums10.length-1)){
-//            System.out.print(i + " ");
-//        }
-//        System.out.println("");
-//
-//        for(int i:quickSort(nums11, 0, nums11.length-1)){
-//            System.out.print(i + " ");
-//        }
-//        System.out.println("");
-//
-//        for(int i:quickSort(nums12, 0, nums12.length-1)){
-//            System.out.print(i + " ");
-//        }
-//        System.out.println("");
-//
-//        for(int i:quickSort(nums13, 0, nums13.length-1)){
-//            System.out.print(i + " ");
-//        }
-//        System.out.println("");
+        for(int i:quickSort(nums10, 0, nums10.length-1)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for(int i:quickSort(nums11, 0, nums11.length-1)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for(int i:quickSort(nums12, 0, nums12.length-1)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for(int i:quickSort(nums13, 0, nums13.length-1)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
 
         for(int i:quickSort(nums14, 0, nums14.length-1)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        System.out.println("\n+=== merge sort in-place ===+\n");
+        int[] nums15 = {5,1,1,2,0,8,9,0};
+        int[] nums16 = {3,9,8,4};
+        int[] nums17 = {};
+        int[] nums18 = {64,25,12,22,11,-10, 78};
+        int[] nums19 = {0,1,2,3,4,5,6,7};
+
+        mergeSort_inPlace(nums15);
+        for(int i:nums15){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        mergeSort_inPlace(nums16);
+        for(int i:nums16){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        mergeSort_inPlace(nums17);
+        for(int i:nums17){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        mergeSort_inPlace(nums18);
+        for(int i:nums18){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        mergeSort_inPlace(nums19);
+        for(int i:nums19){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        System.out.println("\n+=== merge sort ===+\n");
+        int[] nums20 = {5,1,1,2,0,8,9,0};
+        int[] nums21 = {3,9,8,4};
+        int[] nums22 = {};
+        int[] nums23 = {64,25,12,22,11,-10, 78};
+        int[] nums24 = {0,1,2,3,4,5,6,7};
+
+        for(int i:mergeSort(nums20)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for(int i:mergeSort(nums21)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for(int i:mergeSort(nums22)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for(int i:mergeSort(nums23)){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        for(int i:mergeSort(nums24)){
             System.out.print(i + " ");
         }
         System.out.println("");
@@ -115,7 +186,7 @@ public class SortAnArray {
      * 2. queue the lowest number to the back of sorted section
      * 3. keep repeat step 1 and 2 until it covers the whole array (nums.length times)
      * **/
-    // run-time: O(n^2), memory: O(1)
+    // Selection Sort - run-time: O(n^2), memory: O(1)
     public static int[] selectionSort(int[] nums){
         // keep track of sorting list
         int i = 0;
@@ -149,7 +220,7 @@ public class SortAnArray {
      * 2. repeat step 1 until finish compare the while array
      * 3. repeat step 1 and 2 until there is no swap (all element are sorted)
      * **/
-    // run-time: O(n^2), memory: O(1)
+    // Bubble Sort - run-time: O(n^2), memory: O(1)
     public static int[] bubbleSort(int[] nums){
         boolean isAllSorted = false;
 
@@ -173,7 +244,7 @@ public class SortAnArray {
      * 2. keep comparing the target to the elements in sorted list (back to front-most) until find the right position to insert
      * 3. keep repeat step 1 and 2 until an sorted array covers the whole array (nums.length times)
      * **/
-    // run-time: O(n^2), memory: O(1)
+    // Insertion Sort - run-time: O(n^2), memory: O(1)
     public static int[] insertionSort(int[] nums){
         // keep track of the end a sorted list
         int i = 0;
@@ -202,10 +273,11 @@ public class SortAnArray {
     }
 
     /***
+     * 0. May randomly pick a pivot and return its index
      * 1. divide array into left and right partitions
      * 2. recursively call on left and right until entire array is sorted
      * ***/
-    // runtime av: O(nlog(n)), memory: O(1)
+    // Quick Sort in-place - runtime average case: O(nlog(n)), memory: O(1)
     public static int[] quickSort(int[] nums, int start, int end){
         if (start <= end) {
             // divide
@@ -221,6 +293,7 @@ public class SortAnArray {
     }
 
     /***
+     * 0. if random pivot is selected, swap the random with the first element
      * 1. select a pivot (front-most), declare start pointer, and end pointer
      * 2. move start pointer forward and compare the element with pivot
      * 3. if start pointer element is greater than pivot, stop start pointer:
@@ -265,5 +338,110 @@ public class SortAnArray {
         swap(pivotIndex, j, nums);
 
         return j;
+    }
+
+    /***
+     * 1. divide array by half into left and right arrays
+     * 2. recursively call on left and right until 1 element left in each array
+     * 3. merge left and right already sorted arrays from step#2 (1 element array is a already sorted array)
+     * 4. recursively merge left and right array until we get the whole list sorted
+     * ***/
+    // Merge Sort in-place - runtime: O(nlog(n)), memory: O(n)
+    public static void mergeSort_inPlace(int[] nums){
+        // base case return when one element left in an array
+        if(nums.length <= 1){
+            return;
+        }
+
+        // Divide: divide an array into a half: namely left and right
+        int midIndex = (int)Math.floor(nums.length/2);
+        int[] left = Arrays.copyOfRange(nums, 0, midIndex);
+        int[] right = Arrays.copyOfRange(nums, midIndex, nums.length);
+
+        // recursively divide
+        mergeSort_inPlace(left);
+        mergeSort_inPlace(right);
+
+        // Conquer: merge two sorted arrays recursively
+        mergeTwoSortedArrays_inPlace(left, right, nums);
+    }
+
+    /***
+     * a basic 3 pointers algo to merge two sorted arrays
+     * ***/
+    // runtime: O(n), memory: O(1)
+    private static void mergeTwoSortedArrays_inPlace(int[] nums1, int[] nums2, int[] mergedArray){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] > nums2[j]){
+                mergedArray[k++] = nums2[j++];
+            }else{
+                mergedArray[k++] = nums1[i++];
+            }
+        }
+
+        while (j < nums2.length){
+            mergedArray[k++] = nums2[j++];
+        }
+
+        while (i < nums1.length){
+            mergedArray[k++] = nums1[i++];
+        }
+    }
+
+    /***
+     * 1. divide array by half into left and right arrays
+     * 2. recursively call on left and right until 1 element left in each array
+     * 3. merge left and right already sorted arrays from step#2 (1 element array is a already sorted array)
+     * 4. recursively merge left and right array until we get the whole list sorted
+     * ***/
+    // Merge Sort in-place - runtime: O(nlog(n)), memory: O(n)
+    public static int[] mergeSort(int[] nums){
+        // base case when one element left in an array
+        if(nums.length <= 1){
+            return nums;
+        }
+
+        // Divide: divide an array into a half: namely left and right
+        int midIndex = (int)Math.floor(nums.length/2);
+        int[] left = Arrays.copyOfRange(nums, 0, midIndex);
+        int[] right = Arrays.copyOfRange(nums, midIndex, nums.length);
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        // Conquer: merge two sorted arrays recursively
+        return mergeTwoSortedArrays(left, right);
+    }
+
+    /***
+     * a basic 3 pointers algo to merge two sorted arrays
+     * ***/
+    // runtime: O(n), memory: O(n)
+    private static int[] mergeTwoSortedArrays(int[] nums1, int[] nums2){
+        int[] mergedArray = new int[nums1.length + nums2.length];
+        int i = 0; // keep track of nums1
+        int j = 0; // keep track of nums2
+        int k = 0; // keep track of merged array
+
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] > nums2[j]){
+                mergedArray[k++] = nums2[j++];
+            }else{
+                mergedArray[k++] = nums1[i++];
+            }
+        }
+
+        while (j < nums2.length){
+            mergedArray[k++] = nums2[j++];
+        }
+
+        while (i < nums1.length){
+            mergedArray[k++] = nums1[i++];
+        }
+
+        return mergedArray;
     }
 }
