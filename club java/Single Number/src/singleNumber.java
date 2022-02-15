a2b26e06c997394c9c9ccb543cca7ef6d98b5f8c
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2022-02-15 01:49:27 -0800

    java competitive programming questions

--- club java/Single Number/src/singleNumber.java
+++ club java/Single Number/src/singleNumber.java
@@ -1,14 +1,23 @@
-import java.util.Arrays;
+import java.util.*;
 
 // https://leetcode.com/problems/single-number/
 public class singleNumber {
     public static void main(String[] args){
-        int[] x = {2,2,1};
-        int[] y = {4,1,2,1,2};
-        int[] z = {1};
-        System.out.println(singleNumber(x)); // 1
-        System.out.println(singleNumber(y)); // 4
-        System.out.println(singleNumber(z)); // 1
+        System.out.println("\n+=== solution M1 ===+\n");
+        int[] nums1 = {2,2,1};
+        int[] nums2 = {4,1,2,1,2};
+        int[] nums3 = {1};
+        System.out.println(singleNumber_M1(nums1)); // 1
+        System.out.println(singleNumber_M1(nums2)); // 4
+        System.out.println(singleNumber_M1(nums3)); // 1
+
+        System.out.println("\n+=== solution M2 ===+\n");
+        int[] nums4 = {2,2,1};
+        int[] nums5 = {4,1,2,1,2};
+        int[] nums6 = {1};
+        System.out.println(singleNumber_M2(nums4)); // 1
+        System.out.println(singleNumber_M2(nums5)); // 4
+        System.out.println(singleNumber_M2(nums6)); // 1
     }
 
     /*
@@ -19,29 +28,50 @@ public class singleNumber {
 	 * Post-Condition: none
 	 */
     // runtime: O(n(log(n))), memory: O(1)
-    public static int singleNumber(int[] nums) {
-        // if there is only one number then return
-        if(nums.length == 1){
-            return nums[0];
-        }
-
+    public static int singleNumber_M1(int[] nums) {
         // sort to make it easier to handle array
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length-1; i++){
-            // if a pair of two numbers in a sorted array are equal then they both not the answer
+        int i = 0;
+
+        while(i+1 < nums.length){
             if(nums[i] == nums[i+1]){
-                // skip both
-                i++;
+                // skip to next pair
+                i += 2;
             }else{
-                // if a pair is not equal then we know that the former number is the answer
                 return nums[i];
             }
         }
-
-        // because the array has an odd n integers, the last number will not get compared by the above loop
-        // However, we know that there is a garanteed solution, so if the above loop cannot get the answer, then the answer
-        // is garuntee to be the last number
+        // every pair so far contains the two numbers, so the last element must be the answer
         return nums[nums.length-1];
+    }
 
+    /*
+     * Purpose: Find a single number (without other duplicate) in an array
+     * Parameter: an array - all integer elements have a duplicate but only 1 integer does not
+     * Returns: int - the only single integer in the array without duplicate
+     * Pre-Condition: there is always a single integer element in the array
+     * Post-Condition: none
+     */
+    // runtime: O(n), memory: O(n)
+    public static int singleNumber_M2(int[] nums) {
+        // {num, frequency}
+        HashMap<Integer, Integer> seen = new HashMap();
+
+        // get all elements to the map key with value of its frequency
+        for(int num:nums){
+            if(seen.containsKey(num)){
+                seen.put(num, seen.get(num)+1);
+            }else{
+                seen.put(num, 1);
+            }
+        }
+
+        // find an element with a frequency of 1
+        for(Map.Entry<Integer, Integer> data:seen.entrySet()){
+            if(data.getValue() == 1){
+                return data.getKey();
+            }
+        }
+        return -1;
     }
 }
