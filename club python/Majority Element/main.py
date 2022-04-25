232a5ba6487653989375cebc67bf2fe0303caf77
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2022-04-25 03:37:38 -0700

    python competitive programming problems

--- club python/Majority Element/main.py
+++ club python/Majority Element/main.py
@@ -39,6 +39,16 @@ def majorityElement_M2(nums: List[int]) -> int:
     nums.sort()
     return nums[len(nums)//2]
 
+'''
+    Link: https://leetcode.com/problems/majority-element/
+    Purpose: Find the majority element
+    parameter: List[int] nums - an integer
+    return: int ans: the majority element
+    Pre-Condition: n == nums.length
+                 : 1 <= n <= 5 * 104
+                 : -231 <= nums[i] <= 231 - 1
+    Post-Condition: none
+'''
 # moore's voting algo: runtime: O(n), memory: O(1)
 def majorityElement_M3(nums:List[int]):
     count = 1
@@ -56,6 +66,50 @@ def majorityElement_M3(nums:List[int]):
 
     return candidate
 
+'''
+    Link: https://leetcode.com/problems/majority-element/
+    Purpose: Find the majority element
+    parameter: List[int] nums - an integer
+    return: int ans: the majority element
+    Pre-Condition: n == nums.length
+                 : 1 <= n <= 5 * 104
+                 : -231 <= nums[i] <= 231 - 1
+    Post-Condition: none
+'''
+# divide and conquer: runtime O(nlog(n)) and mememory O(1)
+def majorityElement(self, nums: List[int]) -> int:
+    def divide_and_conquer(left=0, right=len(nums) - 1):
+        # base case: if only an element left
+        if left == right:
+            return nums[left]
+
+            # divide
+        mid = left + (right - left) // 2
+        left_partition = divide_and_conquer(left, mid)
+        right_partition = divide_and_conquer(mid + 1, right)
+
+        # conquer
+        # both left and right agree on the majority
+        if left_partition == right_partition:
+            return left_partition
+
+        left_majority_count = 0
+        right_majority_count = 0
+
+        # compare the majority of the left and right
+        for i in range(left, right + 1):
+            if nums[i] == left_partition:
+                left_majority_count += 1
+
+        for i in range(left, right + 1):
+            if nums[i] == right_partition:
+                right_majority_count += 1
+
+        # return the winner side
+        return left_partition if left_majority_count > right_majority_count else right_partition
+
+    return divide_and_conquer()
+
 
 if __name__ == '__main__':
     print("\n === M1 ===\n")
@@ -69,6 +123,11 @@ if __name__ == '__main__':
     print(majorityElement_M2([1]))  # 3
     print(majorityElement_M2([6,6,6,7,7]))  # 6
     print("\n === M3 ===\n")
+    print(majorityElement_M2([2, 2, 1, 1, 1, 2, 2]))  # 2
+    print(majorityElement_M2([3, 2, 3]))  # 3
+    print(majorityElement_M2([1]))  # 3
+    print(majorityElement_M2([6, 6, 6, 7, 7]))  # 6
+    print("\n === M4 ===\n")
     print(majorityElement_M2([2, 2, 1, 1, 1, 2, 2]))  # 2
     print(majorityElement_M2([3, 2, 3]))  # 3
     print(majorityElement_M2([1]))  # 3
