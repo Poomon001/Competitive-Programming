b000a7d16e7df9e9a3c38b22d88a971154001a54
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2022-04-25 03:15:37 -0700

    python competitive programming problems

--- club python/Majority Element/main.py
+++ club python/Majority Element/main.py
@@ -37,27 +37,24 @@ def majorityElement_M1(nums: List[int]) -> int:
 # sort: runtime O(nlog(n)) and mem O(1)
 def majorityElement_M2(nums: List[int]) -> int:
     nums.sort()
-    globalMax = 0
-    counter = 1
-    ans = 0
-    i = 0
+    return nums[len(nums)//2]
 
-    for i in range(len(nums) - 1):
-        # compare the curr with the next ele
-        if nums[i] == nums[i + 1]:
-            # count one if they are in the same
-            counter += 1
-        elif counter > globalMax:
-            # get max and reset counter to 0 if they are not the same
-            ans = nums[i]
-            globalMax = max(counter, globalMax)
-            counter = 1
+# moore's voting algo: runtime: O(n), memory: O(1)
+def majorityElement_M3(nums:List[int]):
+    count = 1
+    candidate = nums[0]
 
-    # we need to check the case i+1 where i is the final index
-    if counter > globalMax:
-        ans = nums[i]
+    for i in range(1, len(nums)):
+        # select a new candidate
+        if count == 0:
+            candidate = nums[i]
 
-    return ans
+        if nums[i] == candidate:
+            count += 1
+        else:
+            count -= 1
+
+    return candidate
 
 
 if __name__ == '__main__':
@@ -71,3 +68,8 @@ if __name__ == '__main__':
     print(majorityElement_M2([3,2,3]))  # 3
     print(majorityElement_M2([1]))  # 3
     print(majorityElement_M2([6,6,6,7,7]))  # 6
+    print("\n === M3 ===\n")
+    print(majorityElement_M2([2, 2, 1, 1, 1, 2, 2]))  # 2
+    print(majorityElement_M2([3, 2, 3]))  # 3
+    print(majorityElement_M2([1]))  # 3
+    print(majorityElement_M2([6, 6, 6, 7, 7]))  # 6
