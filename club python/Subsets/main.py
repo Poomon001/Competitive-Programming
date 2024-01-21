43c36fd3f714ac828ab8e2eb3869fdf43cd094cd
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2024-01-21 01:11:34 -0800

    Python competitive programming questions

--- club python/Subsets/main.py
+++ club python/Subsets/main.py
@@ -10,7 +10,12 @@ from typing import List
                  : All the numbers of nums are unique.
     Post-Condition: none
 '''
-# backtracking solution - runtime: O(n*2^n), memory: O(2^n)
+# backtracking solution - runtime: O(n*2^n), memory: O(2^n) -> we have 2^n subsets each many contain n elements
+# eg.      [1, 2]
+#       /          \
+#     [1]          []
+#   /    \       /    \
+# [1, 2]  [1]  [2]    []
 def subsets(nums: List[int]) -> List[List[int]]:
     subsets = []
     subset = []
@@ -20,9 +25,11 @@ def subsets(nums: List[int]) -> List[List[int]]:
             subsets.append(subset[:])
             return
 
+        # include the num[i] recursively
         subset.append(nums[i])
         dfs(i + 1)
 
+        # exclude the num[i] recursively
         subset.pop()
         dfs(i + 1)
 
@@ -31,9 +38,10 @@ def subsets(nums: List[int]) -> List[List[int]]:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print(subsets([])) # [[]]
-    # print(subsets([1])) # [[1], []]
-    # print(subsets([1, 2])) # [[1, 2], [1], [2], []]
-    print(subsets([1, 2, 3])) # [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []]
+    print(subsets([1, 2, 3]))  # [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []]
+    print(subsets([])) # [[]]
+    print(subsets([1])) # [[1], []]
+    print(subsets([1, 2])) # [[1, 2], [1], [2], []]
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
