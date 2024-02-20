56e610035438be87618904808f152b09ec12bfc4
poomon001 <poomon2542@gmail.com>
2024-02-19 18:43:49 -0800

    Java competitive programming questions

--- club java/Missing Numbers II/src/MissingNumber.java
+++ club java/Missing Numbers II/src/MissingNumber.java
@@ -7,10 +7,23 @@ import java.util.stream.IntStream;
 public class MissingNumber {
     public static void main(String[] args) {
         int[] nums1 = {3,0,1};
-        System.out.println(missingNumber(nums1));
-
         int[] nums2 = {0,1};
-        System.out.println(missingNumber(nums2));
+        int[] nums3 = {9,6,4,2,3,5,7,0,1};
+
+        System.out.println("\n === Solution 1 === \n");
+        System.out.println(missingNumber_M1(nums1)); // 2
+        System.out.println(missingNumber_M1(nums2)); // 2
+        System.out.println(missingNumber_M1(nums3)); // 8
+
+        System.out.println("\n === Solution 1 === \n");
+        System.out.println(missingNumber_M2(nums1)); // 2
+        System.out.println(missingNumber_M2(nums2)); // 2
+        System.out.println(missingNumber_M2(nums3)); // 8
+
+        System.out.println("\n === Solution 1 === \n");
+        System.out.println(missingNumber_M2(nums1)); // 2
+        System.out.println(missingNumber_M2(nums2)); // 2
+        System.out.println(missingNumber_M2(nums3)); // 8
     }
 
     /*
@@ -51,7 +64,7 @@ public class MissingNumber {
     */
 
     // vanilla java: runtime: O(n^2), memory: O(1)
-    public static int missingNumber(int[] nums) {
+    public static int missingNumber_M2(int[] nums) {
         // Chack if there is any missing element
         for(int i = 0; i < nums.length; i++) {
             int num = i;
@@ -61,6 +74,30 @@ public class MissingNumber {
         }
         // if no missing element return the highest number + 1
         return Arrays.stream(nums).max().getAsInt() + 1;
+    }
+
+    /*
+    * Link: https://leetcode.com/problems/missing-number/
+    * Purpose: Find the only number in the range that is missing from the array.
+    * Parameters: int [] - a sequence of integer from o to n
+    * Returns: int - a missing interger in the sequence
+    * Pre-Condition: n == nums.length
+                   : 1 <= n <= 104
+                   : 0 <= nums[i] <= n
+                   : All the numbers of nums are unique.
+      Post-Condition : None
+    */
+
+    // sequential sum: runtime: O(n), memory: O(1)
+    public static int missingNumber_M3(int[] nums) {
+        int actualSum = 0;
+        int expectedSum = (nums.length * (nums.length + 1)) / 2;
+
+        for(int n:nums){
+            actualSum += n;
+        }
+
+        return expectedSum - actualSum;
     }
 
 }
