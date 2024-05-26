1925f3a6b7e17541eb5313da42ccdfedb65fc38a
poomon001 <poomon2542@gmail.com>
2024-05-26 03:52:32 -0700

    Python competitive programming questions

--- club python/Valid Palindrome II/main.py
+++ club python/Valid Palindrome II/main.py
@@ -12,42 +12,18 @@
 def validPalindrome(s: str) -> bool:
     left = 0
     right = len(s) - 1
-    removeOne = False
-
-    # scan from left and right
+    s1 = ""
+    s2 = ""
     while left < right:
         if s[left] != s[right]:
-            if removeOne:
-                return False
-            else:
-                removeOne = True
-                # skip left
-                if isPalindrome(s[left + 1:right + 1]):
-                    left += 1
-                    continue
-                # skip right
-                elif isPalindrome(s[left:right]):
-                    right -= 1
-                    continue
-                else:
-                    return False
-
+            s1 = s[left:right]  # a new substring excluding the char at the right pointer
+            s2 = s[left + 1:right + 1]  # a new substring excluding the char at the left pointer
+            break
         left += 1
         right -= 1
-    return True
 
-
-'''
-Purpose: Determine if the s can be palindrome.
-    parameter: str s - a string
-    return: bool - return true if the s is palindrome. Otherwise false
-    Pre-Condition: 1 <= s.length <= 105
-                 : s consists of lowercase English letters.
-    Post-Condition: none
-'''
-# runtime: O(n), memory: O(1)
-def isPalindrome(s: str) -> bool:
-    return s == s[::-1]
+    # Check if either of the substrings is a palindrome
+    return s1 == s1[::-1] or s2 == s2[::-1]
 
 if __name__ == "__main__":
     print(validPalindrome("aba")) # True
