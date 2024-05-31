25bcaf7b7e7f220e2d456bfaab5e93e21a460acf
poomon001 <poomon2542@gmail.com>
2024-05-31 00:43:01 -0700

    update Python solution

--- club python/Jump Game/main.py
+++ club python/Jump Game/main.py
@@ -11,25 +11,20 @@ from typing import List
     Post-Condition: none
 '''
 
-# greedy - always take the highest distance in the range: runtime O(n), memory O(1)
+# greedy: runtime O(n), memory O(1)
 def jumpGame(nums: List[int]) -> bool:
-    highestCanMoveValue = 0
-
-    for i in range(len(nums)):
-        currValue = nums[i]
-
-        # take highest value
-        highestCanMoveValue = max(currValue, highestCanMoveValue)
-
-        # the CanMoveDistance is 0 that means we cannot move, thus return False
-        # but if it is the final index (i = len(nums)), it is okay to be 0
-        if highestCanMoveValue == 0 and i != len(nums) - 1:
+    greedyFuel = 0
+    for num in nums:
+        if greedyFuel < 0:
             return False
+        else:
+            # greedy pick: current fuel or the new fuel
+            greedyFuel = max(greedyFuel, num)
 
-        # highestCanMoveValue loses 1 for each move
-        highestCanMoveValue -= 1
+        greedyFuel -= 1
 
     return True
+
 
 
 if __name__ == '__main__':
