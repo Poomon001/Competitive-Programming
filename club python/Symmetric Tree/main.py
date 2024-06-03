8f97ba3730e3ef04b9fad80ea3a7ec699ab039f6
poomon001 <poomon2542@gmail.com>
2024-06-03 01:01:06 -0700

    .idea

--- club python/Symmetric Tree/main.py
+++ club python/Symmetric Tree/main.py
@@ -19,7 +19,7 @@ class newNode:
     Post-Condition: none
 '''
 # runtime: O(n), memory: O(n)
-def isSymmetric(root: Optional[TreeNode]) -> bool:
+def isSymmetric_m1(root: Optional[TreeNode]) -> bool:
     left = []
     right = []
 
@@ -56,6 +56,65 @@ def isSymmetric(root: Optional[TreeNode]) -> bool:
 
     return left == right
 
+'''
+    Link: https://leetcode.com/problems/symmetric-tree/
+    Purpose: Determine if a tree is symmertric (mirror image)
+    parameter: Optional[TreeNode] root - a root of a BST
+    return: boolean - true if a tree is symmetric. Otherwise, false.
+    Pre-Condition: The number of nodes in the tree is in the range [1, 1000].
+                 : -100 <= Node.val <= 100
+    Post-Condition: none
+'''
+# runtime: O(n), memory: O(n)
+def isSymmetric_m2(root: Optional[TreeNode]) -> bool:
+    def isMirror(left, right) -> bool:
+        if left is None and right is None:
+            return True
+
+        if left is None or right is None:
+            return False
+
+        if left.val != right.val:
+            return False
+
+        return isMirror(left.left, right.right) and isMirror(left.right, right.left)
+
+    return isMirror(root.left, root.right)
+
+'''
+    Link: https://leetcode.com/problems/symmetric-tree/
+    Purpose: Determine if a tree is symmertric (mirror image)
+    parameter: Optional[TreeNode] root - a root of a BST
+    return: boolean - true if a tree is symmetric. Otherwise, false.
+    Pre-Condition: The number of nodes in the tree is in the range [1, 1000].
+                 : -100 <= Node.val <= 100
+    Post-Condition: none
+'''
+# runtime: O(n), memory: O(n)
+def isSymmetric_m3(root: Optional[TreeNode]) -> bool:
+    ans = True
+
+    def dfs(left, right) -> bool:
+        nonlocal ans
+        if left is None and right is None:
+            return
+
+        if left is None or right is None:
+            ans = False
+            return
+
+        if left and root and left.val != right.val:
+            ans = False
+
+        if left and right:
+            dfs(left.left, right.right)
+
+        if left and right:
+            dfs(left.right, right.left)
+
+    dfs(root.left, root.right)
+    return ans
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     root1 = newNode(1)
@@ -82,9 +141,22 @@ if __name__ == '__main__':
     root4.right.left = newNode(0)
     root4.right.right = newNode(3)
 
-    print(isSymmetric(root1)) # true
-    print(isSymmetric(root2)) # false
-    print(isSymmetric(root3)) # true
-    print(isSymmetric(root4)) # false
+    print("\n === old solution === \n")
+    print(isSymmetric_m1(root1)) # true
+    print(isSymmetric_m1(root2)) # false
+    print(isSymmetric_m1(root3)) # true
+    print(isSymmetric_m1(root4)) # false
+
+    print("\n === recursive solution === \n")
+    print(isSymmetric_m2(root1))  # true
+    print(isSymmetric_m2(root2))  # false
+    print(isSymmetric_m2(root3))  # true
+    print(isSymmetric_m2(root4))  # false
+
+    print("\n === dfs solution === \n")
+    print(isSymmetric_m3(root1))  # true
+    print(isSymmetric_m3(root2))  # false
+    print(isSymmetric_m3(root3))  # true
+    print(isSymmetric_m3(root4))  # false
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
