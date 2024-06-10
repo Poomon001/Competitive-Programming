f08593f8964ab74d61541178d16c1a718a28cb1f
poomon001 <poomon2542@gmail.com>
2024-06-09 19:22:01 -0700

    update Java programming solution

--- club java/Advanced Swap Two Numbers/src/AdvancedSwapTwoNumbers.java
+++ club java/Advanced Swap Two Numbers/src/AdvancedSwapTwoNumbers.java
@@ -8,33 +8,38 @@ public class AdvancedSwapTwoNumbers {
         System.out.println(pair1);
         System.out.println(swap_m1(pair1));
         System.out.println(swap_m2(pair1));
+        System.out.println(swap_m3(pair1));
 
         Pair pair2 = new Pair(6, 0);
         System.out.println("\n === test 2 ===\n");
         System.out.println(pair2);
         System.out.println(swap_m1(pair2));
         System.out.println(swap_m2(pair2));
+        System.out.println(swap_m3(pair2));
 
         Pair pair3 = new Pair(1, -2);
         System.out.println("\n === test 3 ===\n");
         System.out.println(pair3);
         System.out.println(swap_m1(pair3));
         System.out.println(swap_m2(pair3));
+        System.out.println(swap_m3(pair3));
 
         Pair pair4 = new Pair(-1, -2);
         System.out.println("\n === test 4 ===\n");
         System.out.println(pair4);
         System.out.println(swap_m1(pair4));
         System.out.println(swap_m2(pair4));
+        System.out.println(swap_m3(pair4));
 
         Pair pair5 = new Pair(-2, -1);
         System.out.println("\n === test 5 ===\n");
         System.out.println(pair5);
         System.out.println(swap_m1(pair5));
         System.out.println(swap_m2(pair5));
+        System.out.println(swap_m3(pair5));
     }
 
-    // runtime: O(1). memory: O(1) with a temporary storage
+    // temp swap - runtime: O(1), memory: O(1) with a temporary storage
     public static Pair swap_m1(Pair pair){
         int temp = 0;
         int x = pair.getX();
@@ -46,7 +51,7 @@ public class AdvancedSwapTwoNumbers {
         return new Pair(x, y);
     }
 
-    // runtime: O(1). memory: O(1) without a temporary storage
+    // Math - runtime: O(1), memory: O(1) without a temporary storage
     public static Pair swap_m2(Pair pair){
         int x = pair.getX();
         int y = pair.getY();
@@ -57,6 +62,20 @@ public class AdvancedSwapTwoNumbers {
         x = y - x;
         y = y - x;
         x = x + y;
+        return new Pair(x, y);
+    }
+
+    // XOR - runtime: O(1). memory: O(1) without a temporary storage
+    public static Pair swap_m3(Pair pair){
+        int x = pair.getX();
+        int y = pair.getY();
+
+        // a XOR a = 0
+        // a XOR 0 = a
+        // a XOR b XOR a XOR 0 = (a XOR a) XOR (b XOR 0) = b
+        x = x ^ y; // z = x ^ y
+        y = y ^ x; // y ^ (x ^ y) = x
+        x = y ^ x; // x ^ (x ^ y) = y
         return new Pair(x, y);
     }
 }
