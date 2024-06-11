3de58f71f86e4dafd42c15de19b1e1aa9894f68c
poomon001 <poomon2542@gmail.com>
2024-06-10 19:13:51 -0700

    update java programming solution

--- club java/ Reverse String/src/ReversString.java
+++ club java/ Reverse String/src/ReversString.java
@@ -1,15 +1,33 @@
 import java.util.*;
 public class ReversString {
     public static void main(String[] args) {
+        System.out.println("\n === solution 1 === \n");
+
         char[] s1 = "Hello".toCharArray();
         char[] s2 = "hello".toCharArray();
         char[] s3 = "leo".toCharArray();
         char[] s4 = "a".toCharArray();
 
-        reverseString_inplace(s1);
-        reverseString_inplace(s2);
-        reverseString_inplace(s3);
-        reverseString_inplace(s4);
+        reverseString_inplace_m1(s1);
+        reverseString_inplace_m1(s2);
+        reverseString_inplace_m1(s3);
+        reverseString_inplace_m1(s4);
+
+        System.out.println(Arrays.toString(s1)); // [o, l, l, e, H]
+        System.out.println(Arrays.toString(s2)); //[o, l, l, e, h]
+        System.out.println(Arrays.toString(s3)); // [o, e, l]
+        System.out.println(Arrays.toString(s4)); // [a]
+
+        System.out.println("\n === solution 2 === \n");
+        s1 = "Hello".toCharArray();
+        s2 = "hello".toCharArray();
+        s3 = "leo".toCharArray();
+        s4 = "a".toCharArray();
+
+        reverseString_inplace_m2(s1);
+        reverseString_inplace_m2(s2);
+        reverseString_inplace_m2(s3);
+        reverseString_inplace_m2(s4);
 
 
         System.out.println(Arrays.toString(s1)); // [o, l, l, e, H]
@@ -27,7 +45,7 @@ public class ReversString {
      *              : s[i] is a printable ascii character.
      * Post-Condition : the input array is reversed
      **/
-    public static void reverseString_inplace(char[] s) {
+    public static void reverseString_inplace_m1(char[] s) {
         int j = 0;
         for(int i = 0; i < s.length/2; i++){
             j = s.length - 1 - i;
@@ -38,5 +56,17 @@ public class ReversString {
         char temp = s[j];
         s[j] = s[i];
         s[i] = temp;
+    }
+
+    public static void reverseString_inplace_m2(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            s[i] = (char) (s[i] + s[j]);
+            s[j] = (char) (s[i] - s[j]);
+            s[i] = (char) (s[i] - s[j]);
+            i++;
+            j--;
+        }
     }
 }
