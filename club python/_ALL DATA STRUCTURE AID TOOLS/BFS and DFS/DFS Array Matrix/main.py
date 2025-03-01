4c8b49123edd53a6d8343fdf79637222e3e560c0
poomon001 <poomon2542@gmail.com>
2025-03-01 02:55:57 -0800

    Add dfs Array matrix

---
+++ club python/_ALL DATA STRUCTURE AID TOOLS/BFS and DFS/DFS Array Matrix/main.py
@@ -0,0 +1,67 @@
+from typing import List
+from collections import deque
+
+def dfs_matrix(board: List[List[str]]):
+    # add top, bottom, right, left orderly to the stack
+    directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    def dfs(i, j, index):
+        stack = deque()
+        visited = set()
+        stack.append((i, j, index))
+        max_length = 0
+
+        while stack:
+            i, j, index = stack.pop()
+
+            # - there is no blocker,
+            # - not yet visited (No multiple append C-C and C-S e.g [["A", "B", "C", "E"], ["S", "F", "C", "S"])
+            if (i, j) in visited or board[i][j] == "":
+                continue
+
+            visited.add((i, j))
+            print(board[i][j], end="-")
+            max_length = max(index + 1, max_length)
+
+            for y, x in directions:
+                c = i + y
+                r = j + x
+
+                # explore all adjacent entries
+                if 0 <= c < len(board) and 0 <= r < len(board[0]) and (c, r) not in visited:
+                    stack.append((c, r, index + 1))
+
+        print(max_length)
+
+    dfs(0, 0, 0)
+
+
+if __name__ == "__main__":
+    dfs_matrix(
+        [["A", "B", "C", "E"],
+        ["S", "F", "C", "S"],
+        ["A", "D", "E", "E"]]
+    ) # A-B-C-E-S-C-F-S-A-D-E-E-
+    print("")
+    dfs_matrix(
+        [["A", "B", "C", ""],
+         ["S", "F", "C", "S"],
+         ["A", "D", "E", "E"]]
+    ) # A-B-C-C-F-S-A-D-E-E-S-
+    print("")
+    dfs_matrix(
+        [["A", "", "L", "k"],
+        ["", "F", "C", "S"],
+        ["A", "D", "E", "E"]]
+    ) # A-
+    print("")
+    dfs_matrix(
+        [["A", "B", "", "k"],
+         ["", "F", "", "S"],
+         ["A", "D", "", "E"]]
+    ) # A-B-F-D-A-
+    print("")
+    dfs_matrix(
+        [["A", "B", "C", ""],
+         ["S", "F", "", "S"],
+         ["A", "K", "E", "E"]]
+    )  # A-B-C-F-S-A-K-E-E-S-9
