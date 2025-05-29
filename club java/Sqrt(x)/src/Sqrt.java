8cb2ea324608bfe0d858a69d4420f5db1854ed87
poomon001 <poomon2542@gmail.com>
2025-05-29 07:10:35 -0700

    Java competitive programming questions

--- club java/Sqrt(x)/src/Sqrt.java
+++ club java/Sqrt(x)/src/Sqrt.java
@@ -1,8 +1,22 @@
 public class Sqrt {
     public static void main(String[] args) {
-//
-        System.out.println(sqrt(2147395600)); // 46340
-        System.out.println(sqrt(2147483647)); // 46340
+        System.out.println("\n === Solution 1 === \n");
+        System.out.println(sqrt_M1(2147395600)); // 46340
+        System.out.println(sqrt_M1(2147483647)); // 46340
+        System.out.println(sqrt_M1(101)); // 10
+        System.out.println(sqrt_M1(1)); // 1
+        System.out.println(sqrt_M1(0)); // 0
+        System.out.println(sqrt_M1(8)); // 2
+        System.out.println(sqrt_M1(4)); // 2
+
+        System.out.println("\n === Solution 2 === \n");
+        System.out.println(sqrt_M2(2147395600)); // 46340
+        System.out.println(sqrt_M2(2147483647)); // 46340
+        System.out.println(sqrt_M2(101)); // 10
+        System.out.println(sqrt_M2(1)); // 1
+        System.out.println(sqrt_M2(0)); // 0
+        System.out.println(sqrt_M2(8)); // 2
+        System.out.println(sqrt_M2(4)); // 2
     }
 
     /**
@@ -14,24 +28,49 @@ public class Sqrt {
      * Post-Condition : None
      * Requirment: using pow(x, 0.5) is not permitted
      **/
-    // runtime: O(n), memory: O(1)
-    public static int sqrt(int x){
-        // store the previous possible solution
-        int prev = 0;
-
-        // find i where i*i is close to x from left side
-        for(long i = 0; i <= x; i++){
-            if(i*i == x) {
-                // if i*i is x, the answer is i
-                return (int) i;
-            }else if(i*i > x){
-                // if i*i > x, the answer is previous i
-                return prev;
+    // runtime: O(n*n^2), memory: O(1)
+    public static int sqrt_M2(int x){
+        int num = x;
+        for(int i = 0; i <= num; i++) {
+            long pow = (long) i * i;
+            if(pow >= x){
+                if(pow == x) {
+                    return i;
+                }
+                return i - 1;
             }
-            prev = (int) i;
         }
 
-        // should never reach this line
-        return -1;
+        return 0;
+    }
+
+    /**
+     * Link: https://leetcode.com/problems/sqrtx/
+     * Purpose: Find a floor of sqrt of x
+     * Parameters: int x - an integer
+     * Returns: int - a floor integer sqrt of x
+     * Pre-Condition: 0 <= x <= 231 - 1
+     * Post-Condition : None
+     * Requirment: using pow(x, 0.5) is not permitted
+     **/
+    // runtime: O(log(n)*n^2), memory: O(1)
+    public static int sqrt_M1(int x){
+        int i = 0;
+        int j = x;
+
+        while(i <= j){
+            int mid = (j - i) / 2 + i;
+            long pow = (long) mid * mid;
+            if(pow == x) {
+                return mid;
+            }
+            if(pow > x) {
+                j = mid -1;
+            }else{
+                i = mid +1;
+            }
+        }
+
+        return j;
     }
 }
