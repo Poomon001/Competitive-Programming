d5650be38a224e1e8bfffb6fe2f9260e465e001f
poomon001 <poomon2542@gmail.com>
2025-07-13 09:42:35 -0700

    Java competitive programming question - topic: queue

--- club java/Moving Average from Data Stream/src/MovingAverageDataStream.java
+++ club java/Moving Average from Data Stream/src/MovingAverageDataStream.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MovingAverageDataStream {
     public static void main(String[] args) {
-        System.out.println("\n total objects:" + MovingAverage.sumObj + "\n total nexts:" + MovingAverage.sumNext);
+        System.out.println("\n === Solution 1 === \n");
+        System.out.println("\n total objects:" + MovingAverage_M1.sumObj + "\n total nexts:" + MovingAverage_M1.sumNext);
 
-        MovingAverage obj1 = new MovingAverage(3);
+        MovingAverage_M1 obj1 = new MovingAverage_M1(3);
         System.out.println(obj1.next(2)); // 2
         System.out.println(obj1.next(5)); // 3.5
         System.out.println(obj1.next(5)); // 4
@@ -11,9 +15,9 @@ public class MovingAverageDataStream {
         System.out.println(obj1.next(12)); // 6.667
         System.out.println(obj1.next(3)); // 6
 
-        System.out.println("\n total objects:" + MovingAverage.sumObj + "\n total nexts:" + MovingAverage.sumNext);
+        System.out.println("\n total objects:" + MovingAverage_M1.sumObj + "\n total nexts:" + MovingAverage_M1.sumNext);
 
-        MovingAverage obj2 = new MovingAverage(1);
+        MovingAverage_M1 obj2 = new MovingAverage_M1(1);
         System.out.println(obj2.next(2)); // 2
         System.out.println(obj2.next(5)); // 5
         System.out.println(obj2.next(5)); // 5
@@ -22,9 +26,9 @@ public class MovingAverageDataStream {
         System.out.println(obj2.next(12)); // 12
         System.out.println(obj2.next(3)); // 3
 
-        System.out.println("\n total objects:" + MovingAverage.sumObj + "\n total nexts:" + MovingAverage.sumNext);
+        System.out.println("\n total objects:" + MovingAverage_M1.sumObj + "\n total nexts:" + MovingAverage_M1.sumNext);
 
-        MovingAverage obj3 = new MovingAverage(10);
+        MovingAverage_M1 obj3 = new MovingAverage_M1(10);
         System.out.println(obj3.next(2)); // 2
         System.out.println(obj3.next(5)); // 3.5
         System.out.println(obj3.next(5)); // 4
@@ -33,7 +37,44 @@ public class MovingAverageDataStream {
         System.out.println(obj3.next(12)); // 5.333
         System.out.println(obj3.next(3)); // 5
 
-        System.out.println("\n total objects:" + MovingAverage.sumObj + "\n total nexts:" + MovingAverage.sumNext);
+        System.out.println("\n total objects:" + MovingAverage_M1.sumObj + "\n total nexts:" + MovingAverage_M1.sumNext);
+
+        System.out.println("\n === Solution 2 === \n");
+
+        System.out.println("\n total objects:" + MovingAverage_M2.sumObj + "\n total nexts:" + MovingAverage_M2.sumNext);
+
+        MovingAverage_M2 obj4 = new MovingAverage_M2(3);
+        System.out.println(obj4.next(2)); // 2
+        System.out.println(obj4.next(5)); // 3.5
+        System.out.println(obj4.next(5)); // 4
+        System.out.println(obj4.next(5)); // 5
+        System.out.println(obj4.next(3)); // 4.333
+        System.out.println(obj4.next(12)); // 6.667
+        System.out.println(obj4.next(3)); // 6
+
+        System.out.println("\n total objects:" + MovingAverage_M2.sumObj + "\n total nexts:" + MovingAverage_M2.sumNext);
+
+        MovingAverage_M2 obj5 = new MovingAverage_M2(1);
+        System.out.println(obj5.next(2)); // 2
+        System.out.println(obj5.next(5)); // 5
+        System.out.println(obj5.next(5)); // 5
+        System.out.println(obj5.next(5)); // 5
+        System.out.println(obj5.next(3)); // 3
+        System.out.println(obj5.next(12)); // 12
+        System.out.println(obj5.next(3)); // 3
+
+        System.out.println("\n total objects:" + MovingAverage_M2.sumObj + "\n total nexts:" + MovingAverage_M2.sumNext);
+
+        MovingAverage_M2 obj6 = new MovingAverage_M2(10);
+        System.out.println(obj6.next(2)); // 2
+        System.out.println(obj6.next(5)); // 3.5
+        System.out.println(obj6.next(5)); // 4
+        System.out.println(obj6.next(5)); // 4.25
+        System.out.println(obj6.next(3)); // 4
+        System.out.println(obj6.next(12)); // 5.333
+        System.out.println(obj6.next(3)); // 5
+
+        System.out.println("\n total objects:" + MovingAverage_M2.sumObj + "\n total nexts:" + MovingAverage_M2.sumNext);
     }
 
     /**
@@ -46,7 +87,7 @@ public class MovingAverageDataStream {
                   : At most 10^4 calls will be made to next.
      Post-Condition: none
      **/
-    public static class MovingAverage {
+    public static class MovingAverage_M1 {
         public static int sumObj = 0;
         public static int sumNext = 0;
         int[] window;
@@ -55,7 +96,7 @@ public class MovingAverageDataStream {
         int currSize = 0;
 
         // Initializes the object with the size of the window size
-        public MovingAverage(int size) {
+        public MovingAverage_M1(int size) {
             this.window = new int[size];
             this.sumObj++;
         }
@@ -79,5 +120,32 @@ public class MovingAverageDataStream {
             return this.total / currSize;
         }
     }
+
+    public static class MovingAverage_M2 {
+        public static int sumObj = 0;
+        public static int sumNext = 0;
+        private int size;
+        private Queue<Integer> nums;
+        private double sum;
+
+        public MovingAverage_M2(int size) {
+            this.size = size;
+            this.nums = new LinkedList<>();
+            this.sum = 0;
+            this.sumObj++;
+        }
+
+        public double next(int val) {
+            this.sumNext++;
+            if (nums.size() == size) {
+                sum -= nums.poll();
+            }
+
+            nums.add(val);
+            sum += val;
+            return sum / nums.size();
+        }
+    }
+
 }
 
