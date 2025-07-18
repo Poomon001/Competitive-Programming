7df838b9f6452731ebf031c78dedeffef9947ffd
poomon001 <poomon2542@gmail.com>
2025-07-18 02:01:54 -0700

    Java competitive programming questions - topic: stack

--- club java/Maximum Nesting Depth of the Parentheses/src/MaximumNestingDepthOfTheParentheses.java
+++ club java/Maximum Nesting Depth of the Parentheses/src/MaximumNestingDepthOfTheParentheses.java
@@ -7,6 +7,17 @@ public class MaximumNestingDepthOfTheParentheses {
         System.out.println(maxDepth("aaaaa")); // 0
     }
 
+    /**
+     Link: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+     Purpose: Given a valid parentheses string s, find the nesting depth is the maximum number of nested parentheses.
+     parameter: String s - a valid parentheses string
+     return: int maxCount - the maximum number of nested parentheses
+     Pre-Condition: 1 <= s.length <= 100
+                  : s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
+                  : It is guaranteed that parentheses expression s is a VPS.
+     Post-Condition: none
+     **/
+    // rumtime: O(n), memory: O(n)
     public static int maxDepth(String s) {
         Stack<Character> myStack = new Stack<>();
         int count = 0;
