10e871a2d8c5356752824621d7f4ffd6f5dd052a
poomon001 <poomon2542@gmail.com>
2025-09-13 13:51:22 -0700

    Python competitive programming question - topic: two pointers

--- club python/Move Zeros/main.py
+++ club python/Move Zeros/main.py
@@ -6,58 +6,66 @@ from typing import List
     parameter: List[int] - alist of integer
     return: List[int] - a list of integer where all 0's are at the end
     Pre-Condition: 1 <= nums.length <= 104
-                 : -231 <= nums[i] <= 231 - 1
+                 : -2^31 <= nums[i] <= 2^31 - 1
     Post-Condition: none
 '''
-# memory: O(n) and runtime: O(n)
+# in-place swap: runtime: O(n), memory: O(1)
 def moveZeroes_M1(nums: List[int]) -> List:
-        newNums = []
-        numZero = 0
+    # 1. create 2 pointer: i and j
+    # 2. i will keep track of 0's
+    # 3. j will keep track of non-0's
+    # 4. i and j needs to be swapped if j is non-zeros
 
-        # append only non-zero
-        for num in nums:
-            if num == 0:
-                numZero += 1
-            else:
-                newNums.append(num)
+    i = 0  # keep track of the left-most 0
 
-        #append zero at the end
-        for i in range(numZero):
-            newNums.append(0)
+    for j in range(len(nums)):
+        while i < len(nums) and nums[i] != 0:
+            i += 1
 
-        return newNums[:]
-
+        if nums[j] != 0 and i < j:
+            nums[j], nums[i] = nums[i], nums[j] # swap
+    return nums
 '''
     Link: https://leetcode.com/problems/move-zeroes/
     Purpose: Move all 0's to the end of it while maintaining the relative order of the non-zero elements.
     parameter: List[int] - alist of integer
     return: List[int] - a list of integer where all 0's are at the end
     Pre-Condition: 1 <= nums.length <= 104
-                 : -231 <= nums[i] <= 231 - 1
+                 : -2^31 <= nums[i] <= 2^31 - 1
     Post-Condition: none
 '''
-# memory: O(1) and runtime: O(n)
+# in-place overwrite: runtime: O(n), memory: O(1)
 def moveZeroes_M2(nums: List[int]) -> List:
     # keep track of non-zero int
     j = 0
-    length = len(nums)
 
-    for i in range(length):
-        # if int is non-zero queue from the front
+    for i in range(len(nums)):
+        # if int is non-zero, overwrite it at the front
         if nums[i] != 0:
             nums[j] = nums[i]
             j += 1
 
-    # after j non-zero int to the end of array, put 0's
-    for i in range(j, length):
+    # put 0's from the j index to the end of array
+    for i in range(j, len(nums)):
         nums[i] = 0
 
     return nums
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(moveZeroes_M1([0, 1, 0, 3, 12]))  # [1, 3, 12, 0, 0]
-    print(moveZeroes_M2([0,1,0,3,12])) # [1, 3, 12, 0, 0]
-    print(moveZeroes_M1([0]))  # [0]
-    print(moveZeroes_M2([0])) # [0]
+    print("\n ==== Solution M1 ==== \n")
+    print(moveZeroes_M1([1, 1, 3, 12, 2])) # [1, 1, 3, 12, 2]
+    print(moveZeroes_M1([1])) # [1]
+    print(moveZeroes_M1([0])) # [0]
+    print(moveZeroes_M1([0, 1, 0, 3, 12])) # [1, 3, 12, 0, 0]
+    print(moveZeroes_M1([0, 0, 0, 0, 1, 0, 3, 12])) # [1, 3, 12, 0, 0, 0, 0, 0]
+    print(moveZeroes_M1([1, 0, 0, 0, 0, 3, 0, 0, 0, 12, 0])) # [1, 3, 12, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    print("\n ==== Solution M2 ==== \n")
+    print(moveZeroes_M2([1, 1, 3, 12, 2]))  # [1, 1, 3, 12, 2]
+    print(moveZeroes_M2([1]))  # [1]
+    print(moveZeroes_M2([0]))  # [0]
+    print(moveZeroes_M2([0, 1, 0, 3, 12]))  # [1, 3, 12, 0, 0]
+    print(moveZeroes_M2([0, 0, 0, 0, 1, 0, 3, 12]))  # [1, 3, 12, 0, 0, 0, 0, 0]
+    print(moveZeroes_M2([1, 0, 0, 0, 0, 3, 0, 0, 0, 12, 0]))  # [1, 3, 12, 0, 0, 0, 0, 0, 0, 0, 0]
 
