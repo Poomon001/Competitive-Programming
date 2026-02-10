b502ed574e4df75db40d2aa1e6ec80846096326b
poomon001 <poomon2542@gmail.com>
2026-02-10 10:28:04 -0800

    Java competitive programming question - topic: binary search

--- club java/Search a 2D Matrix/src/Search2DMatrix.java
+++ club java/Search a 2D Matrix/src/Search2DMatrix.java
@@ -1,16 +1,29 @@
 public class Search2DMatrix {
     public static void main(String[] args) {
         int[][] matrix1 = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        System.out.println(searchMatrix(matrix1, 3)); // true
-        System.out.println(searchMatrix(matrix1, 13)); // false
-
         int[][] matrix2 = new int[][]{{2,3,5,7},{10,11,16,20},{23,30,34,60},{61,130,314,610}};
-        System.out.println(searchMatrix(matrix2, 13)); // false
-        System.out.println(searchMatrix(matrix2, 1)); // false
-        System.out.println(searchMatrix(matrix2, 1000)); // false
-        System.out.println(searchMatrix(matrix2, 2)); // true
-        System.out.println(searchMatrix(matrix2, 610)); // true
-        System.out.println(searchMatrix(matrix2, 20)); // true
+
+        System.out.println("\n === solution 1 \n");
+        System.out.println(searchMatrix_M1(matrix1, 3)); // true
+        System.out.println(searchMatrix_M1(matrix1, 13)); // false
+
+        System.out.println(searchMatrix_M1(matrix2, 13)); // false
+        System.out.println(searchMatrix_M1(matrix2, 1)); // false
+        System.out.println(searchMatrix_M1(matrix2, 1000)); // false
+        System.out.println(searchMatrix_M1(matrix2, 2)); // true
+        System.out.println(searchMatrix_M1(matrix2, 610)); // true
+        System.out.println(searchMatrix_M1(matrix2, 20)); // true
+
+        System.out.println("\n === solution 2 \n");
+        System.out.println(searchMatrix_M2(matrix1, 3)); // true
+        System.out.println(searchMatrix_M2(matrix1, 13)); // false
+
+        System.out.println(searchMatrix_M2(matrix2, 13)); // false
+        System.out.println(searchMatrix_M2(matrix2, 1)); // false
+        System.out.println(searchMatrix_M2(matrix2, 1000)); // false
+        System.out.println(searchMatrix_M2(matrix2, 2)); // true
+        System.out.println(searchMatrix_M2(matrix2, 610)); // true
+        System.out.println(searchMatrix_M2(matrix2, 20)); // true
     }
 
     /**
@@ -26,7 +39,7 @@ public class Search2DMatrix {
         Post-Condition: none
     **/
     // 2-level binary search - runtime: O(log(m) + log(n)), memory: O(1)
-    public static boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix_M1(int[][] matrix, int target) {
         // search for the possible row
         int top = 0;
         int bottom = matrix.length - 1;
@@ -63,6 +76,40 @@ public class Search2DMatrix {
             }
         }
 
+        return false;
+    }
+
+    public static boolean searchMatrix_M2(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        /**
+         * [[1, 3, 5],
+         * [7, 9, 11],
+         * [13, 15, 17]]
+         * [1, 3, 5, 7, 9, 11, 13, 15, 17]
+         *  l                           r
+        **/
+        int left = 0;
+        int right = n * m - 1;
+
+        while(left <= right) {
+            int mid = ((right - left) / 2) + left;
+
+            // 2D search trick
+            int row = mid / m;
+            int col = mid % m;
+            int value = matrix[row][col];
+
+            if (target == value) {
+               return true;
+            }
+            if (target > value) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return false;
     }
 }
