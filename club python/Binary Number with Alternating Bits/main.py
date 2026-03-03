df266cb9c92c3cb3553fdfa8ef2bb6519cfbef4a
poomon001 <poomon2542@gmail.com>
2026-03-03 01:58:56 -0800

    Python competitive programming question - topic: bit manipulation

---
+++ club python/Binary Number with Alternating Bits/main.py
@@ -0,0 +1,27 @@
+'''
+    Link: https://leetcode.com/problems/binary-number-with-alternating-bits
+    Purpose: Find if the number has alternating bits
+    parameter: int n - a positive number
+    return: bool - True if the number has alternating bits. Otherwise, False
+    Pre-Condition: 1 <= n <= 2^31 - 1
+    Post-Condition: none
+'''
+def hasAlternatingBits(n: int) -> bool:
+    mask = 1  # 0000 0001
+    temp = mask & n
+    n = n >> 1
+    while n > 0:
+        last = mask & n
+        if last == temp:
+            return False
+        temp = last
+        n = n >> 1
+    return True
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(hasAlternatingBits(1)) # True
+    print(hasAlternatingBits(5)) # True
+    print(hasAlternatingBits(7)) # False
+    print(hasAlternatingBits(11)) # False
+    print(hasAlternatingBits(1000)) # False
