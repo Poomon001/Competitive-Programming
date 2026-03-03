097f6cd7892e9fb1233aaa2c135165e69ed72644
poomon001 <poomon2542@gmail.com>
2026-03-03 02:19:21 -0800

    Python competitive programming question - topic: tree

--- club python/Sum of Root To Leaf Binary Numbers/main.py
+++ club python/Sum of Root To Leaf Binary Numbers/main.py
@@ -18,29 +18,25 @@ class newNode:
                  : Node.val is 0 or 1.
     Post-Condition: none
 '''
-# example of misuse left and right val that is solved by passing a parameter
-# value passing in a parameter will not calculate in this recursion but wait until next recursion
 # runtime: O(n), memory: O(depth of tree)
 def sumRootToLeaf_M1(root: Optional[TreeNode]) -> int:
-    sumRoot = 0
+    ans = 0
 
-    def dfs(root, num):
-        nonlocal sumRoot
+    def dfs(root: Optional[TreeNode], bits: str) -> int:
+        nonlocal ans
+        # if None
+        if not root:
+            return
 
-        if root.left:
-            ''' this line is incorrect b/c the code add left val before a recursive call, so when returning the num will already be changed '''
-            # num = num + str(root.left.val)
-            # dfs(root.left, num)
-            dfs(root.left, num + str(root.left.val))
+            # if leaf
+        if not root.left and not root.right:
+            ans += int(bits + str(root.val), 2)
 
-        if root.right:
-            dfs(root.right, num + str(root.right.val))
+        dfs(root.left, bits + str(root.val))
+        dfs(root.right, bits + str(root.val))
 
-        if root and not root.left and not root.right:
-            sumRoot += int(num, 2)
-
-    dfs(root, str(root.val))
-    return sumRoot
+    dfs(root, "")
+    return ans
 
 
 '''
@@ -52,7 +48,6 @@ def sumRootToLeaf_M1(root: Optional[TreeNode]) -> int:
                  : Node.val is 0 or 1.
     Post-Condition: none
 '''
-# example of misuse left and right val that is solved by using root.val
 # runtime: O(n), memory: O(depth of tree)
 def sumRootToLeaf_M2(root: Optional[TreeNode]) -> int:
     sumRoot = 0
@@ -63,18 +58,73 @@ def sumRootToLeaf_M2(root: Optional[TreeNode]) -> int:
         num = num + str(root.val)
 
         if root.left:
-            ''' this line is incorrect b/c the code add left val before a recursive call, so when returning the num will already be changed '''
-            # num = num + str(root.left.val)
             dfs(root.left, num)
 
         if root.right:
             dfs(root.right, num)
 
+        # leaf node
         if root and not root.left and not root.right:
             sumRoot += int(num, 2)
 
     dfs(root, "")
     return sumRoot
+
+'''
+    Link: https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+    Purpose: Find a sum of all binary left leaves
+    parameter: Optional[TreeNode] root - a root of a binary tree
+    return: int sum - a sum of all binary left leaves
+    Pre-Condition: The number of nodes in the tree is in the range [1, 1000].
+                 : Node.val is 0 or 1.
+    Post-Condition: none
+'''
+# runtime: O(n), memory: O(depth of tree)
+def sumRootToLeaf_M3(root: Optional[TreeNode]) -> int:
+    def dfs(root: Optional[TreeNode], bits: str) -> int:
+        # if None
+        if not root:
+            return 0
+
+        # if leaf
+        if not root.left and not root.right:
+            return int(bits + str(root.val), 2)
+
+        return dfs(root.left, bits + str(root.val)) + dfs(root.right, bits + str(root.val))
+
+    return dfs(root, "")
+
+'''
+    Link: https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+    Purpose: Find a sum of all binary left leaves
+    parameter: Optional[TreeNode] root - a root of a binary tree
+    return: int sum - a sum of all binary left leaves
+    Pre-Condition: The number of nodes in the tree is in the range [1, 1000].
+                 : Node.val is 0 or 1.
+    Post-Condition: none
+'''
+# ADVANCED math - runtime: O(n), memory: O(depth of tree)
+def sumRootToLeaf_M4(root: Optional[TreeNode]) -> int:
+    ans = 0
+
+    def dfs(root: Optional[TreeNode], curr_sum: str) -> int:
+        nonlocal ans
+        # if None
+        if not root:
+            return
+
+            # 1 0 1 = (1 * 2^2) + (0 * 2^1) + (1 * 2^0) = (1)*2*2 + (0)*2 + (1)
+        curr_sum = curr_sum * 2 + root.val
+
+        # if leaf
+        if not root.left and not root.right:
+            ans += curr_sum
+
+        dfs(root.left, curr_sum)
+        dfs(root.right, curr_sum)
+
+    dfs(root, 0)
+    return ans
 
 if __name__ == '__main__':
     root1 = newNode(1)
@@ -115,6 +165,20 @@ if __name__ == '__main__':
     print(sumRootToLeaf_M2(root3))  # 5
     print(sumRootToLeaf_M2(root4))  # 7
     print(sumRootToLeaf_M2(root5))  # 11
+
+    print("\n+=== Solution3 ===+\n")
+    print(sumRootToLeaf_M3(root1))  # 22
+    print(sumRootToLeaf_M3(root2))  # 0
+    print(sumRootToLeaf_M3(root3))  # 5
+    print(sumRootToLeaf_M3(root4))  # 7
+    print(sumRootToLeaf_M3(root5))  # 11
+
+    print("\n+=== Solution4 ===+\n")
+    print(sumRootToLeaf_M4(root1))  # 22
+    print(sumRootToLeaf_M4(root2))  # 0
+    print(sumRootToLeaf_M4(root3))  # 5
+    print(sumRootToLeaf_M4(root4))  # 7
+    print(sumRootToLeaf_M4(root5))  # 11
 
 
 
